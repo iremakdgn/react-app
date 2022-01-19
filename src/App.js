94cb6a67faeb735856b456718ced13dc06872a99
iremakdgn <irem.akdgn.1996@gmail.com>
2022-01-19 20:55:13 +0300

    jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header'; //COMPONENETLERİ BAŞ HARFİ BÜYÜK OLACAK ŞEKİLDE ALMALIYIZ
 
 function App() {
 
     //return React.createElement("div", null, "hello");
+    // Eğerki div kullanmadan yapmak istiyorsak <></> sadece şu şekil kullanabiliriz. Bunun adı da fragmenttir.
     return ( < div >
         <
         h1 > Hello React < /h1> <
-        Header / > < /
-        div > )
-}
+        Header / >
+        <
+        p className = 'text' >
+        Lorem ipsum lorem ipsum lorem ipsum < /p> <
+        label htmlFor = 'myinput' >
+        Name <
+        input id = 'myinput' > < /input> < /
+        label > <
+        /div > )
+    }
 
-export default App;
+    export default App;

bbbd95080f6ec9c2f56a4859b9db583f8bf2803e
iremakdgn <irem.akdgn.1996@gmail.com>
2022-01-19 21:03:58 +0300

    koşullu render

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'; //COMPONENETLERİ BAŞ HARFİ BÜYÜK OLACAK ŞEKİLDE ALMALIYIZ
 
+const name = "Mehmet";
+const isLoggedIn = false;
+
 function App() {
 
     //return React.createElement("div", null, "hello");
     // Eğerki div kullanmadan yapmak istiyorsak <></> sadece şu şekil kullanabiliriz. Bunun adı da fragmenttir.
     return ( < div >
         <
-        h1 > Hello React < /h1> <
-        Header / >
-        <
-        p className = 'text' >
-        Lorem ipsum lorem ipsum lorem ipsum < /p> <
-        label htmlFor = 'myinput' >
-        Name <
-        input id = 'myinput' > < /input> < /
-        label > <
-        /div > )
-    }
+        h1 > { isLoggedIn ? name : "Giriş yapmadınız" } < /h1>  < /
+        div > )
+}
 
-    export default App;
+export default App;

6778866d138bb324ba54246a007ebeed2b979e66
iremakdgn <irem.akdgn.1996@gmail.com>
2022-01-20 20:01:25 +0300

    shapre,oneoftype,defaultprops

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function App() {
     //return React.createElement("div", null, "hello");
     // Eğerki div kullanmadan yapmak istiyorsak <></> sadece şu şekil kullanabiliriz. Bunun adı da fragmenttir.
     return ( <div>
-        <User name = "Mehmet" surname = "Seven" isLoggedIn = { false } friends={["Ahmet","Tayfun","Gökhan"]}/>  </div> )
+        <User name = "Mehmet" surname = "Seven" age={24} isLoggedIn = { true } 
+        friends={["Ahmet","Tayfun","Gökhan"]}
+        addres={{
+            title:"Ataşehir/İstanbul",
+            zip:34755
+        }}/>  </div> )
     }
 
     export default App;
--- src/components/User.js
+++ src/components/User.js
@@ -1,21 +1,38 @@
 import PropTypes from 'prop-types';
 
-function User({ name, surname, isLoggedIn, friends }) {
+function User({ name, surname, age, isLoggedIn, friends, address }) {
     return (
     <> 
-        <h1> { isLoggedIn ? `${name}` `${surname}` : "Giriş yapmadınız" } </h1>
+        <h1> { isLoggedIn ? `${name} ${surname} ${age}`  : "Giriş yapmadınız" } </h1>
         {
             friends.map((friend,index)=> <div key={index}>{index}-{friend}</div>)
+
+            
         }
+        {address.title} {address.zip}
     </>
     )
 }
 
 User.prototypes={
-    name:PropTypes.string,
+    name:PropTypes.string.isRequired, //prop tiplerinde zorunlu alanları belirlemek için isrequired
     surname:PropTypes.string,
+    age:PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string
+    ]).isRequired,
     isLoggedIn:PropTypes.bool,
-    friends:PropTypes.array
+    friends:PropTypes.array,
+    address: PropTypes.shape({
+        title:PropTypes.string,
+        zip:PropTypes.number
+    })
 }
 
+User.defaultProps={ isLoggedIn:false}
+   
+
+
 export default User;
+
+//oneoftype: bir propda birden fazla veri tipi göndermek için
